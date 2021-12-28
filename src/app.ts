79b2f3d8f2b256429f04470bd061e248e797e2fb
DenisBindyukov <gitgroom@gmail.com>
2021-12-28 12:10:35 +0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import fastify, {FastifyServerOptions} from "fastify";
+
+const buildApp = (options: FastifyServerOptions) => {
+    const app = fastify(options);
+
+    return app;
+};
+
+
+export default buildApp;
